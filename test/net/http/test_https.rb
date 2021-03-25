00422fb4ce72b30ac82e3b46df4d9c2cb0df43fd
David Rodríguez <deivid.rodriguez@riseup.net>
2021-03-25 12:04:05 +0100

    Restore ruby 2.5 support
    As far as I know, this is the only stdlib that has dropped ruby 2.5,
    which hasn't yet reached its End of Life date.
    
    The motivation for this is that there's no gemified version of
    `net-http` that supports ruby 2.5, and that makes it hard for libraries
    depending on `net-http` to properly declare the dependency, since the
    only way is to drop support for ruby 2.5, which many maintainers will
    not yet want to do.
    
    The reason for wanting to declare the `net-http` dependency is mainly
    that it helps bundler & rubygems with avoiding double loads of different
    versions of `net-http` (the stdlib version and a higher gemified version).
    
    As far as I can observe from commit history, the only reason for
    dropping support for ruby 2.5 was that tests didn't pass, so I made them
    pass.

--- test/net/http/test_https.rb
+++ test/net/http/test_https.rb
@@ -45,7 +45,7 @@ class TestNetHTTPS < Test::Unit::TestCase
       assert_equal($test_net_http_data, res.body)
     }
     # TODO: OpenSSL 1.1.1h seems to yield only SERVER_CERT; need to check the incompatibility
-    certs.zip([CA_CERT, SERVER_CERT][-certs.size..]) do |actual, expected|
+    certs.zip([CA_CERT, SERVER_CERT][-certs.size..-1]) do |actual, expected|
       assert_equal(expected.to_der, actual.to_der)
     end
   rescue SystemCallError
@@ -66,7 +66,7 @@ class TestNetHTTPS < Test::Unit::TestCase
       assert_equal($test_net_http_data, res.body)
     }
     # TODO: OpenSSL 1.1.1h seems to yield only SERVER_CERT; need to check the incompatibility
-    certs.zip([CA_CERT, SERVER_CERT][-certs.size..]) do |actual, expected|
+    certs.zip([CA_CERT, SERVER_CERT][-certs.size..-1]) do |actual, expected|
       assert_equal(expected.to_der, actual.to_der)
     end
   end
